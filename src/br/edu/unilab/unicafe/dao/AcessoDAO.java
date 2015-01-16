a433b4aa52944dc06ebba2672bc288c136506e07
Jefferson Uchoa Ponte <jefponte@gmail.com>
2015-01-16 15:52:52 +0000

    Instalador

---
+++ src/br/edu/unilab/unicafe/dao/AcessoDAO.java
@@ -0,0 +1,14 @@
+package br.edu.unilab.unicafe.dao;
+
+public class AcessoDAO extends DAO{
+	
+	
+	public AcessoDAO(){
+		super();
+	}
+	public AcessoDAO(int tipoDeConexao){
+		super(tipoDeConexao);
+		
+	}
+	
+}
