742446b788b3a2dae3de146a9f01feca902723e1
Erivando Sena <erivandosena@gmail.com>
2015-02-26 13:23:51 +0000

    Correções

---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+import br.edu.unilab.unicafe.view.FrameClientDesbloqueado;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		FrameClientDesbloqueado janela = new FrameClientDesbloqueado();
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.setVisible(true);
+
+	}
+
+}
