99196c33af35e30254f6ccd14e89665a36fdb50a
Erivando Sena <erivandosena@gmail.com>
2015-02-26 13:16:39 +0000

    Correções

---
+++ src/br/edu/unilab/unicafe/main/MainTesteFrames.java
@@ -0,0 +1,13 @@
+package br.edu.unilab.unicafe.main;
+
+import br.edu.unilab.unicafe.model.Servidor;
+
+public class MainTesteFrames {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Servidor servidor = new Servidor();
+		servidor.iniciaSplash();
+	}
+
+}
