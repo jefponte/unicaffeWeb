d1d649d2cca4e9319078cd00fba1d8dbb4072a14
Jefferson Uchoa Ponte <jefponte@gmail.com>
2015-05-13 01:23:56 +0000

    Telas Terminadas.

---
+++ src/main/java/br/edu/unilab/unicafe/view/FrameSplash.java
@@ -0,0 +1,29 @@
+package br.edu.unilab.unicafe.view;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+
+public class FrameSplash extends JFrame {
+	private JPanel contentPane;
+	public FrameSplash() {
+		
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(145, 216, 247));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setSize(800, 350);
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(getClass().getResource(UtilFrames.BASE_PATH_IMAGES + "unicafe-logo.png")));
+		label.setBounds(100, 107, 600, 136);
+		contentPane.add(label);
+		setLocationRelativeTo(null);  
+		setUndecorated(true);
+		
+	}
+	
+}
