9b2b931fa2b31cd5061305ccffce8ac0a7a1e117
Jefferson Uchoa Ponte <jefponte@gmail.com>
2015-06-11 21:43:12 -0300

    Começando a comitar unicafeweb.

---
+++ src/main/java/br/edu/unilab/unicafe/main/MainUpdate.java
@@ -0,0 +1,12 @@
+package br.edu.unilab.unicafe.main;
+
+import br.edu.unilab.unicafe.update.Update;
+
+public class MainUpdate {
+
+	public static void main(String[] args) {
+		Update update = new Update();
+		update.iniciaUpdate();
+		
+	}
+}
