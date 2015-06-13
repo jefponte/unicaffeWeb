b1341ffb555f57811eb688f911b9e58221963aa8
Jefferson Uchoa Ponte <jefponte@gmail.com>
2015-06-12 21:43:46 -0300

    Organizando estrutura de diretórios para apresentação dos componentes do sistema no git.

---
+++ projeto/unicafeWin/unicafe/src/main/java/br/edu/unilab/unicafe/dao/DAO.java
@@ -0,0 +1,143 @@
+package br.edu.unilab.unicafe.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 
+ * @author Jefferson
+ *
+ */
+public class DAO {
+	
+	private int tipoDeConexao;
+	public void setTipoDeConexao(int tipoDeConexao){
+		this.tipoDeConexao = tipoDeConexao;
+	}
+	
+	public DAO(int tipoDeConexao) {
+		this.tipoDeConexao = tipoDeConexao;
+		novaConexao();
+	}
+
+	public DAO() {
+		this.tipoDeConexao = TIPO_CONEXAO_DEFAULT;
+		novaConexao();
+	}
+
+	public DAO(Connection conexao) {
+		this.conexao = conexao;
+	}
+
+	public void novaConexao(){
+		this.conexao = null;
+		try {
+			switch (tipoDeConexao) {
+			case TIPO_POSTGRESQL:
+				Class.forName(DRIVER_POSTGRES);
+				this.conexao = DriverManager.getConnection(JDBC_BANCO_POSTGRES+ "//" + HOST_POSTGRES + "/" + BANCO_POSTGRES,USUARIO_POSTGRES, SENHA_POSTGRES);
+				break;
+			case TIPO_PG_PRODUCAO:
+				Class.forName(DRIVER_POSTGRES);
+				this.conexao = DriverManager.getConnection(JDBC_BANCO_POSTGRES+ "//" + HOST_PG_PRODUCAO + "/" + BANCO_PG_PRODUCAO,USUARIO_PG_PRODUCAO, SENHA_PG_PRODUCAO);
+				break;
+			case TIPO_PG_SIGAA:
+				Class.forName(DRIVER_POSTGRES);
+				this.conexao = DriverManager.getConnection(JDBC_BANCO_POSTGRES+ "//" + HOST_PG_SIGAA + "/" + BANCO_PG_SIGAA,USUARIO_PG_SIGAA, SENHA_PG_SIGAA);
+				break;
+			case TIPO_PG_TESTE:
+				Class.forName(DRIVER_POSTGRES);
+				this.conexao = DriverManager.getConnection(JDBC_BANCO_POSTGRES+ "//" + HOST_PG_TESTE+ "/" + BANCO_PG_TESTE,USUARIO_PG_TESTE, SENHA_PG_TESTE);
+				break;
+				
+			case TIPO_PG_SIMULACAO_SIGAA:
+				Class.forName(DRIVER_POSTGRES);
+				this.conexao = DriverManager.getConnection(JDBC_BANCO_POSTGRES+ "//" + HOST_PG_SIMULACAO_SIGAA+ "/" + BANCO_PG_SIMULACAO_SIGAA,USUARIO_PG_SIMULACAO_SIGAA, SENHA_PG_SIMULACAO_SIGAA);
+				break;
+			case TIPO_MYSQL:
+				Class.forName(DRIVER_MYSQL);
+				this.conexao=DriverManager.getConnection(JDBC_BANCO_MYSQL+"//"+IP_MYSQL+"/"+BANCO_MYSQL,USUARIO_MYSQL,SENHA_MYSQL);
+				break;
+			case TIPO_SQLITE:
+				Class.forName(DRIVER_SQLITE);
+				this.conexao = DriverManager.getConnection(JDBC_BANCO_SQLITE);
+				break;
+			default:
+				Class.forName(DRIVER_SQLITE);
+				this.conexao = DriverManager.getConnection(JDBC_BANCO_SQLITE);
+				break;
+			}
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private Connection conexao;
+
+	public Connection getConexao() {
+		return conexao;
+	}
+
+	public void setConexao(Connection conexao) {
+		this.conexao = conexao;
+	}
+
+	
+
+	public static final String USUARIO_POSTGRES = "postgres";
+	public static final String USUARIO_PG_SIGAA = "unicafe";
+	public  static final String USUARIO_PG_TESTE = "unicafe";
+	public  static final String SENHA_PG_SIGAA = "unicafe";
+	public  static final String SENHA_PG_SIMULACAO_SIGAA = "unicafe@unilab";
+	public static final String USUARIO_PG_SIMULACAO_SIGAA = "unicafe";
+	public static final String USUARIO_PG_PRODUCAO = "unicafe";
+	public  static final String HOST_POSTGRES = "localhost:5432";
+	public  static final String HOST_PG_PRODUCAO = "localhost:5432";
+	public  static final String HOST_PG_SIGAA = "200.129.19.80";
+	public  static final String HOST_PG_TESTE = "10.5.1.8";
+	public  static final String HOST_PG_SIMULACAO_SIGAA = "10.5.1.8:5432";
+	
+	public  static final String BANCO_PG_SIGAA = "sistemas_comum";
+	public  static final String BANCO_PG_TESTE = "unicafe";
+	public  static final String BANCO_PG_SIMULACAO_SIGAA = "sistemas_comum";
+	public  static final String BANCO_PG_PRODUCAO = "unicafe";
+	
+	public  static final String SENHA_POSTGRES = "cti@unilab2012";
+	public  static final String SENHA_PG_PRODUCAO = "unicafe";
+	public  static final String SENHA_PG_TESTE= "unicafe@unilab";
+
+	public  static final String BANCO_POSTGRES = "unicafe";
+	public  static final String DRIVER_POSTGRES = "org.postgresql.Driver";
+	public  static final String JDBC_BANCO_POSTGRES = "jdbc:postgresql:";
+
+	public static final String DRIVER_SQLITE = "org.sqlite.JDBC";
+	public static final String JDBC_BANCO_SQLITE = "jdbc:sqlite:banco.db";
+
+	
+	public static final String USUARIO_MYSQL = "root";
+	public static final String SENHA_MYSQL = "cocacola@12";
+	public static final String IP_MYSQL = "127.0.0.1";
+	public static final String BANCO_MYSQL = "banco_sigaa";
+	public static final String DRIVER_MYSQL = "com.mysql.jdbc.Driver";
+	public static final String JDBC_BANCO_MYSQL = "jdbc:mysql:";
+
+	
+	
+	public static final int TIPO_SQLITE = 0;
+	public static final int TIPO_MYSQL = 1;
+	public static final int TIPO_POSTGRESQL = 2;
+	public static final int TIPO_PG_SIGAA = 4;
+	public static final int TIPO_PG_TESTE = 5;
+	public static final int TIPO_PG_SIMULACAO_SIGAA = 6;
+	
+	public static final int TIPO_PG_PRODUCAO = 7;
+	
+	public static final int TIPO_CONEXAO_DEFAULT = TIPO_PG_TESTE;
+	public static final int TIPO_CONEXAO_AUTENTICACAO = TIPO_PG_SIMULACAO_SIGAA;
+	
+	
+
+}
---
+++ projeto/unicafeWin/unicafe/src/main/java/br/edu/unilab/unicafe/dao/MaquinaDAO.java
@@ -0,0 +1,116 @@
+package br.edu.unilab.unicafe.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import br.edu.unilab.unicafe.model.Maquina;
+
+public class MaquinaDAO extends DAO{
+	
+	public MaquinaDAO(){
+		super();
+	}
+	public MaquinaDAO(int tipoDeConexao){
+		super(tipoDeConexao);
+		
+	}
+	public MaquinaDAO(Connection conexao){
+		super(conexao);
+	}
+	
+	public ArrayList<Maquina>retornaLista(){
+		ArrayList<Maquina>lista = new ArrayList<Maquina>();
+		PreparedStatement ps;
+		try {
+			ps = this.getConexao().prepareStatement("SELECT * FROM maquina");
+			ResultSet resultSet = ps.executeQuery();
+			while(resultSet.next()){
+				Maquina maquina = new Maquina();
+
+				maquina.setNome(resultSet.getString("nome_pc"));
+				maquina.setId(resultSet.getInt("id_maquina"));
+				lista.add(maquina);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return lista;
+	}
+	
+	/**
+	 * Apaga todo o registro de acesso.
+	 */
+	public void limpa(){
+		try {
+			PreparedStatement ps = this.getConexao().prepareStatement("DELETE FROM maquina");
+			ps.executeQuery();
+			
+		} catch (SQLException e) {
+		}		
+	}
+
+	/**
+	 * Retorna True se a m�quina existir. Verifica a partir do atributo nome da m�quina. 
+	 * 
+	 * @param maquina
+	 * @return
+	 */
+	public boolean existe(Maquina maquina){
+		try {
+			PreparedStatement ps = this.getConexao().prepareStatement("SELECT * FROM maquina WHERE nome_pc = ?");
+			ps.setString(1, maquina.getNome());
+			
+			
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				maquina.setId(rs.getInt("id_maquina"));
+				return true;
+			}
+			return false;					
+			
+		} catch (SQLException e) {
+			return false;
+		}		
+		
+	}
+	/**
+	 * Retorna true se a m�quina for cadastrada com sucesso. 
+	 * @param maquina
+	 * @return
+	 */
+
+	public boolean cadastra(Maquina maquina){
+		try {
+			PreparedStatement ps = this.getConexao().prepareStatement("SELECT * FROM maquina WHERE nome_pc = ?");
+			ps.setString(1, maquina.getNome());
+			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+				return false;
+			PreparedStatement ps2 = this.getConexao().prepareStatement("INSERT into maquina(nome_pc, mac) VALUES(?, ?)");			
+			ps2.setString(1, maquina.getNome());
+			ps2.setString(2, "Não Informado");
+			ps2.executeUpdate();
+			
+			PreparedStatement ps3 = this.getConexao().prepareStatement("SELECT * FROM maquina WHERE nome_pc = ?");
+			ps3.setString(1, ""+maquina.getNome());
+			ResultSet rs2 = ps3.executeQuery();
+			while(rs2.next()){
+				maquina.setId(rs2.getInt("id_maquina"));
+				
+			}
+			return true;					
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}		
+	}
+
+}
