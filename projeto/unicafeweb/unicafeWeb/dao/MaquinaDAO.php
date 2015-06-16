d4b3c918397f8137f37af814721a441f1d54f407
Jefferson Uchoa Ponte <jefponte@gmail.com>
2015-06-15 22:14:38 -0300

    UniCafeSantment é um objeto que contem uma matriz com os dados em tempo de execução. Tudo está sendo constrolado pelo DAO.

---
+++ projeto/unicafeweb/unicafeWeb/dao/MaquinaDAO.php
@@ -0,0 +1,20 @@
+<?php
+
+class MaquinaDAO extends DAO{
+	
+	public function retornaMaquinas(){
+		$sql = "SELECT * FROM maquina";
+		echo $sql;
+		$stmt = $this->getConexao()->query($sql);
+		$listaDeMaquinas = array();
+		foreach($stmt as $elemento){
+
+			$maquina = new Maquina();
+			$maquina->setNome($elemento['nome']);
+			$listaDeMaquinas[] = $maquina;
+			
+		}
+		return $listaDeMaquinas;
+	}
+	
+}
