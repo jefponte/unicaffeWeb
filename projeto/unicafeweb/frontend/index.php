21065f857f052ec2490a4dbb339e4f47eec231c0
Jefferson Uchoa Ponte <jefponte@gmail.com>
2015-07-29 22:27:42 -0300

    Modificações no Menu - By Olinda.

--- projeto/unicafeweb/frontend/index.php
+++ projeto/unicafeweb/frontend/index.php
@@ -1,79 +1,114 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-	<head>	
-		<meta charset="UTF-8">
-    	<title>Unicaffé Web</title>
-    	<link rel="stylesheet" href="http://spa.dsi.unilab.edu.br/spa/css/spa.css" />
-    	<link rel="stylesheet" href="css/style.css" />
-	</head>
-	<body>
-		<div class="pagina doze colunas">
-			<div class="topo doze linha fundo-branco">
-				<div class="conteudo">
-					<div id="logo-unicaffe">
-        				<a href="#">
-							<img alt="logotipo do Unicaffé" src="img/logo_unicaffe.png" title="Ir para Unicaffé">
-						</a>
-				    </div>
-    				<div id="logo-universidade">
-        				<a href="#">
-							<img alt="logotipo da Unilab" src="img/logo_unilab.png" title="Ir para Unilab">
-						</a>
-					</div>
-				</div>
-    		</div>
-    		<div class="unicaffe-menu">
-        		<ol>
-	            	<li><a href="#">Inicío</a></li>
-		            <li><a href="#">Laboratório</a></li>
-		            <li><a href="#">Máquinas</a>
-		                <ul>
-		                    <li><a href="#">Subitem (1º Nível)</a>
-		                        <ul>
-		                            <li><a href="#">Subitem (2º Nível)</a>
-		                                <ul>
-		                                    <li><a href="#">Subitem (3º Nível) ...</a></li>
-		                                    <li><a href="#">Subitem com texto muito extenso</a></li>
-		                                </ul>
-		                            </li>
-		                            <li><a href="#">Com ícone</a></li>
-		                            <li><a href="#">Com subitens</a>
-		                                <ul>
-		                                    <li><a href="#">Lista não ordenada</a></li>
-		                                    <li><a href="#">dentro de um item</a></li>
-		                                    <li><a href="#">cria novos níveis</a></li>
-		                                </ul>
-		                            </li>
-		                        </ul>
-		                    </li>
-		                </ul>
-		            </li>
-		            <li><a href="#">Gerenciamento</a>
-		                <ul>
-		                    <li><a href="#">Subitem (1º Nível)</a>
-		                        <ul>
-		                            <li><a href="#">Subitem (2º Nível)</a>
-		                                <ul>
-		                                    <li><a href="#">Subitem (3º Nível) ...</a></li>
-		                                    <li><a href="#">Subitem com texto muito extenso</a></li>
-		                                </ul>
-		                            </li>
-		                            <li><a href="#">Com ícone</a></li>
-		                            <li><a href="#">Com subitens</a>
-		                                <ul>
-		                                    <li><a href="#">Lista não ordenada</a></li>
-		                                    <li><a href="#">dentro de um item</a></li>
-		                                    <li><a href="#">cria novos níveis</a></li>
-		                                </ul>
-		                            </li>
-		                        </ul>
-		                    </li>
-		                </ul>
-		            </li>
-		            <li class="a-direita"><a href="#" class="ativo">Login</a></li>
-        		</ol>
-    		</div>
-		</div>
-	</body>
+    <head>	
+        <meta charset="UTF-8">
+        <title>Unicaffé Web</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" /><!-- meta tag para responsividade em Windows e Linux -->  
+        <link rel="stylesheet" href="http://spa.dsi.unilab.edu.br/spa/css/spa.css" />
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+        <div class="pagina doze colunas">
+            <div class="topo doze linha fundo-branco">
+                <div class="conteudo">
+                    <div id="logo-unicaffe">
+                        <a href="#">
+                            <img alt="logotipo do Unicaffé" src="img/logo_unicaffe.png" title="Ir para Unicaffé">
+                        </a>
+                    </div>
+                    <div id="logo-universidade">
+                        <a href="#">
+                            <img alt="logotipo da Unilab" src="img/logo_unilab.png" title="Ir para Unilab">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="unicaffe-menu">
+                <ol>
+                    <li><a href="#">Inicío</a></li>
+                    <li><a href="#">Laboratórios</a></li>
+                    <li><a href="#">Máquinas</a>
+                        <ul class="seta-pra-cima">
+                            <li><a href="#" class="ativo">Listagem</a></li>
+                            <li><a href="#">Cadastro</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Gerenciamento</a>
+                        <ul class="seta-pra-cima">
+                            <li><a href="#">Subitem (1º Nível)</a>
+                                <ul>
+                                    <li><a href="#">Subitem (2º Nível)</a>
+                                        <ul>
+                                            <li><a href="#">Subitem (3º Nível) ...</a></li>
+                                            <li><a href="#">Subitem com texto muito extenso</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Com ícone</a></li>
+                                    <li><a href="#">Com subitens</a>
+                                        <ul>
+                                            <li><a href="#">Lista não ordenada</a></li>
+                                            <li><a href="#">dentro de um item</a></li>
+                                            <li><a href="#">cria novos níveis</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="a-direita"><a href="#" class="ativo">Login</a></li>
+                </ol>
+            </div>
+            <div class="linha fundo-marrom1" >
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <h>Tela 01</h>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+            </div>
+            <div class="linha fundo-branco" >
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <h>Tela 02</h>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+            </div>
+            <div class="linha fundo-marrom1" >
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <h>Tela 03</h>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+            </div>
+        </div>
+    </body>
 </html>
 <?php
