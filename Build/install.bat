c5dfdcd9898066ee04efa1c57bdf9da678b69ecc
Jefferson Uchoa Ponte <jefponte@gmail.com>
2016-03-11 10:16:12 -0300

    Removendo tudo para poder ajeitar o gitignore.

--- Build/install.bat
+++
@@ -1,5 +0,0 @@
-echo Antes de Continuar você precisa instalar esta versão do java. 
-net user unicafe /delete
-net user unicafe unicafe@unilab /add /expires:never
-net localgroup administradores unicafe /add
-
--- projeto/unicafeweb/unicafeWeb/visao/sair.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-
-$sessao = new Sessao();
-$sessao->mataSessao();
-
-?>
