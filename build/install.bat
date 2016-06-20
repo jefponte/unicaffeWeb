d20279e33026069ccebb501bd3692b0d575dad21
Jefferson Uchoa Ponte <jefponte@gmail.com>
2016-06-20 11:54:51 -0300

    Teste.

--- build/install.bat
+++ build/install.bat
@@ -1,5 +1,6 @@
 net user unicafe /delete
 net user unicafe unicafe@unilab /add
 net user unicafe /expires:never
+net accounts /maxpwage:unlimited
 net accounts 
 net localgroup administradores unicafe /add
