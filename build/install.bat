e5c14a6287cbb231d89f607ac9ab7a123c4d478a
Jefferson Uchoa Ponte <jefponte@gmail.com>
2016-06-20 13:10:03 -0300

    La vai.

--- build/install.bat
+++ build/install.bat
@@ -2,5 +2,4 @@ net user unicafe /delete
 net user unicafe unicafe@unilab /add
 net user unicafe /expires:never
 net accounts /maxpwage:unlimited
-net accounts 
 net localgroup administradores unicafe /add
