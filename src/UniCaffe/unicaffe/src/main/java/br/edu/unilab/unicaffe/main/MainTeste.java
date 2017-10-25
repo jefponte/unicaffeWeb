452e04b56cfc57a682693de27490e146bb4c1aed
Jefferson Uchoa Ponte <jefponte@gmail.com>
2017-10-25 16:08:01 -0300

    Logo LABPATI alterada para o branco no Cliente.

--- src/UniCaffe/unicaffe/src/main/java/br/edu/unilab/unicaffe/main/MainTeste.java
+++ src/UniCaffe/unicaffe/src/main/java/br/edu/unilab/unicaffe/main/MainTeste.java
@@ -1,8 +1,10 @@
 package br.edu.unilab.unicaffe.main;
 
 import java.io.File;
+import java.io.IOException;
 
 import br.edu.unilab.unicaffe.controller.ClienteController;
+import br.edu.unilab.unicaffe.desktop.Desktop;
 import br.edu.unilab.unicaffe.view.FrameMensagem;
 
 public class MainTeste {
@@ -12,6 +14,32 @@ public class MainTeste {
 		FrameMensagem frame = new FrameMensagem();
 		frame.setVisible(true);
 
+		
+		ClienteController controller = new ClienteController();
+		
+		controller.desBloqueandoServicos();
+		String caminho = "c:\\arquivos";
+		Desktop d = new Desktop(caminho, "jefponte");
+		d.desfazer();
+		try {
+			Runtime.getRuntime().exec(" taskkill /f /im explorer.exe");
+			Thread.sleep(3000);
+			// System.out.println("Abrindo Explorer. ");
+			Runtime.getRuntime().exec("explorer.exe");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.exit(0);
+
+		
+		
+		
+		
 	}
 
 }
