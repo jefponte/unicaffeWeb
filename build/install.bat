615362fdff80e617347c439ab5fb95ddcbc9ff34
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-11-06 09:20:33 -0300

    Separate web application

--- build/install.bat
+++
@@ -1,7 +0,0 @@
-net user unicafe /delete
-net user unicafe unicafe@unilab /add
-net user unicafe /expires:never
-net accounts /maxpwage:unlimited
-net localgroup administradores unicafe /add
-wmic useraccount where "Name='unicafe'" set PasswordExpires=false
-jabswitch /enable
--- src/UniCaffeWeb/js/jquery.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-
-	$('img').width($('.tres colunas').width()).height($(window).height());
-	$(window).resize(function(event) {
-		$('img').width($(window).width()).height($(window).height());
-	});
-});
